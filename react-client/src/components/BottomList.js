9f756ae2b2ee7c2a2d265ba10e170dff11b7b929
Peter Wang <happypeter1983@gmail.com>
2017-06-09 15:42:53 +0800

    add signup/login Link

--- react-client/src/components/BottomList.js
+++ react-client/src/components/BottomList.js
@@ -1,10 +1,39 @@
 import '../css/bottom-list.css'
-import React from 'react'
+import React, { Component } from 'react'
 
-const BottomList = (props) => (
-  <div onClick={props.toggleOpen} 
-  style={props.display ? { display: 'block' } : { display: 'none' }}className="bottom-list">
-  </div>
-)
+class BottomList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showSignUp: false,
+      showLogin: false
+    }
+    this.showLogin = this.showLogin.bind(this)
+    this.showSignUp = this.showSignUp.bind(this)
+  }
+
+  showSignUp() {
+    console.log('showSignUp')
+  }
+
+  showLogin() {
+    console.log('showLogin')
+  }
+
+  render() {
+    return(
+      <div onClick={this.props.toggleOpen}
+      style={this.props.display ? { display: 'block' } : { display: 'none' }}className="bottom-list">
+
+        <div className="login-actions">
+          <ul>
+            <li onClick={this.showSignUp}>注册</li>
+            <li onClick={this.showLogin}>登录</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default BottomList
