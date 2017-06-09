1b9044227b5d26c4a0c38dca7e4582aa35b61f01
Peter Wang <happypeter1983@gmail.com>
2017-06-09 15:07:47 +0800

    more icon button

---
+++ react-client/src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './css/header.css'
+
+class Header extends Component {
+  render() {
+    return(
+      <div className="header">
+        Header
+        <div className="button"></div>
+      </div>
+    )
+  }
+}
+
+export default Header
